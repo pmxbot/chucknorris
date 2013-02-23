d71912df63e1612598b1aa084b4fe322b00cc357
Jason R. Coombs <jaraco@jaraco.com>
2013-02-22 20:12:21 -0500

    Copied norris implementation from yg.irc

---
+++ chucknorris/pmxbot.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+
+from pmxbot.core import command
+from pmxbot.karma import Karma
+
+from . import quips
+import pronoun
+
+@command('norris', aliases=('',),
+    doc='Chuck Norris has a website dedicated to pmxbot facts.')
+def norrisquote(client, event, channel, nick, rest):
+    if rest:
+        rest = rest.strip()
+        Karma.store.change(rest, 2)
+        rcpt = rest
+    else:
+        rcpt = channel
+    return random_quote(rcpt)
+
+def random_quote(nick=None):
+    return pronoun.pronounify(quips.random(), 'Chuck Norris', nick)
+
+
+def init():
+    pronoun.load_nicks(pmxbot.config)
---
+++ chucknorris/quips.py
@@ -0,0 +1,12 @@
+import random as _random
+
+import pkg_resources
+
+from . import pronoun
+
+_quips = [quip.rstrip() for quip in
+    pkg_resources.resource_stream('chucknorris', 'quips.txt')
+    if quip.rstrip()]
+
+def random(name=None):
+    return pronoun.pronounify(_random.choice(_quips), 'Chuck Norris', name)
