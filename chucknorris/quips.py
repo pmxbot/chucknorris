b5687c211a3f607e7641c1eb5a32a88434cce978
Jason R. Coombs <jaraco@jaraco.com>
2023-08-25 11:40:14 -0400

    ⚫ Fade to black.

--- chucknorris/quips.py
+++ chucknorris/quips.py
@@ -4,9 +4,12 @@ import pkg_resources
 
 from . import pronoun
 
-_quips = [quip.rstrip().decode('utf-8') for quip in
-    pkg_resources.resource_stream('chucknorris', 'quips.txt')
-    if quip.rstrip()]
+_quips = [
+    quip.rstrip().decode('utf-8')
+    for quip in pkg_resources.resource_stream('chucknorris', 'quips.txt')
+    if quip.rstrip()
+]
+
 
 def random(name=None):
     return pronoun.pronounify(_random.choice(_quips), 'Chuck Norris', name)
